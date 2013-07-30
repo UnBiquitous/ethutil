3b9294ad570b4b7bd8fe1a125ff9226f335dbc7c
Fabricio Buzeto <nuk.anime13@gmail.com>
2013-07-30 14:12:49 -0400

    Removed getIndex dependency

--- src/br/unb/cic/ethutil/EthUtil.java
+++ src/br/unb/cic/ethutil/EthUtil.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
-import java.net.EthUtilNetworkInterfaceHelper;
 import java.net.Socket;
 import java.net.SocketException;
 import java.net.UnknownHostException;
